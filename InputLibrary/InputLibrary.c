4cc5eabbde4393f2bec017bc4226e5f7c19867de
OsamaSany <osamasny1998@gmail.com>
2019-12-04 13:27:12 +0500

    Input Library Initial Commit

---
+++ InputLibrary/InputLibrary.c
@@ -0,0 +1,12 @@
+// InputLibrary.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "InputLibrary.h"
+
+// TODO: This is an example of a library function
+void fnInputLibrary()
+{
+	OutputDebugString("here");
+}
